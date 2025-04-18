55e911d2bcf638c0307d3301472ddba4afe08a43
Karim Latiris <latiriskarim7@gmail.com>
2025-04-18 21:54:01 +0200

    allow all origins

--- main.py
+++ main.py
@@ -17,12 +17,13 @@ limiter = Limiter(key_func=get_remote_address, default_limits=["10/minute"])
  
 app = FastAPI()
 
-origins = [
-  "https://localhost:4200",
-]
+#origins = [
+#  "https://localhost:4200",
+#]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
