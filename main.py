985365888acf23734c7fad3365b7589b7feb1f0f
Karim Latiris <latiriskarim7@gmail.com>
2025-04-18 21:36:18 +0200

    test

--- main.py
+++ main.py
@@ -20,17 +20,15 @@ app = FastAPI()
 origins = [
   "http://localhost:4200",
 ]
-app.state.limiter = limiter
-app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(SlowAPIMiddleware)
 
 get_secrets()
